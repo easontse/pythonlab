e0e1c3421b71676d559b0857046dfccaa0d480bb
eason <eason.tse@eason-tse.me>
2015-11-28 03:06:35 +0800

    ptt-crawler.py updated

--- crawler/ptt-crawler.py
+++ crawler/ptt-crawler.py
@@ -7,30 +7,28 @@ def ifExist(pageStatus):
 
 def crawlData():
     for i in range(0,len(soup.select('.title'))):
-        print(getYear(i+10)[-4:] + '/' +
-            soup.select('.date')[i].text.strip() + ',' +
-            soup.select('.author')[i].text.strip() + ',' + 
-            "'" + soup.select('.title')[i].text.strip() + "'")
+        data=(crawl_another_page(i+10)[0][-4:] + '/' +
+              soup.select('.date')[i].text.strip() + ',' +
+              crawl_another_page(i+10)[0][-13:-5] + ',' +
+              soup.select('.author')[i].text.strip() + ',' + 
+              soup.select('.title')[i].text.strip() + ',' +
+              crawl_another_page(i+10)[1])
+        print(data.split(',')[::])
     i += 1
 
-def getYear(x):
-    try:
-        soup.select('a')[x]['href']
-    except IndexError:
-        return '0001'
+def crawl_another_page(x):
     yurl = 'https://www.ptt.cc' + str(soup.select('a')[x]['href'])
     yres = requests.get(yurl)
     ysoup = BeautifulSoup(yres.text,"lxml")
-    try:
-        return ysoup.select('.article-meta-value')[3].text
-    except IndexError:
-        return '0000'
-    
-        
-    
-indexNum=1175
-#while indexNum <= 1180:
-while True:
+    if ifExist(res.status_code):
+        try:
+            return [ysoup.select('.article-meta-value')[3].text, yres.url]
+        except IndexError:
+            return ['00:00:00 0000', yres.url]
+            
+indexNum=1
+while indexNum <= 200:
+#while True:
     url = 'https://www.ptt.cc/bbs/Yunlin/index' + str(indexNum) + '.html'
     res = requests.get(url)
     soup = BeautifulSoup(res.text,"lxml")
