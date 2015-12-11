c4e2c9214fe1867e01479909d31b977fc08a9669
admin <admin>
2015-12-12 00:13:42 +0800

    updated

--- crawler/autohome_crawler.py
+++ crawler/autohome_crawler.py
@@ -1,21 +1,22 @@
-import requests, datetime, time, csv
+import requests, time, csv
 from bs4 import BeautifulSoup
 
 def get_soup(indexNum):
-    index_url = 'http://club.autohome.com.cn/bbs/allforums-dateline-9-' +str(indexNum)+ '.html'
+    index_url = 'http://club.autohome.com.cn/bbs/forum-c-3293-' +str(indexNum)+ '.html'
+    print('Trying to request: ' + index_url)
     index_res = requests.get(index_url)
     index_res.encoding = 'gbk'
+    print('Requested sucessfully: ' + index_res.url)
     return BeautifulSoup(index_res.text)
 
 def get_linklist(soup):
     print('generating link list...')
     linklist = []
-    for link in soup.select('.list_dl'):
-        if 'href' in str(link.select('a')[0]):
-            linklist.append('http://club.autohome.com.cn' + link.select('a')[0]['href'])
+    for link in soup.select('dt'):
+        if 'thread' in str(link.a) and 'threadqa' not in str(link.a):
+            linklist.append('http://club.autohome.com.cn' + link.a['href'])
     print('list generated!')
-    print(len(list(set(linklist))))
-    return sorted(list(set(linklist)))
+    return list(set(linklist))
 
 def crawl_thread(thread_url):
     
@@ -23,7 +24,7 @@ def crawl_thread(thread_url):
     thread_res.encoding = 'gbk'
     thread_soup = BeautifulSoup(thread_res.text)
     
-    print('crawling data... ' + thread_res.url)
+    print('crawling 1st page of thread: ' + thread_res.url)
     
     thread_data = []
     for i in thread_soup.find_all('div', {'class':'clearfix'}):
@@ -43,7 +44,6 @@ def crawl_thread(thread_url):
             
         except:
             continue
-    print(len(thread_data))
     
     if len(thread_soup.select('.fs')[1].text) == 6:
         maxPage = int(thread_soup.select('.fs')[1].text[-3])
@@ -52,8 +52,10 @@ def crawl_thread(thread_url):
     elif len(thread_soup.select('.fs')[1].text) == 8:
         maxPage = int(thread_soup.select('.fs')[1].text[-5:-2])
     
+    print('maxPage: ' + str(maxPage))
+    
     sub_linklist = []
-    while maxPage > 1:
+    while maxPage >= 2:
         url = str(thread_url[:-6]) + str(maxPage) + '.html'
         if url not in sub_linklist:
             sub_linklist.append(url)
@@ -91,17 +93,17 @@ def crawl_thread(thread_url):
 pageNum = 1    
 while True:
     try:
+        datalinklist = get_linklist(get_soup(pageNum))
+        counter = 0
         with open('autohome.csv', 'a', encoding='utf-8') as csvfile:
-            datalinklist = get_linklist(get_soup(pageNum))
-            counter = 0
             while counter < len(datalinklist):
                 for data in crawl_thread(datalinklist[counter]):
-                    
                     writer = csv.writer(csvfile, delimiter=',')
                     writer.writerow([data[0],data[1],data[2],data[3],data[4]])
+                    
                     #print(data[0],data[1],data[2],data[3],i)
                     counter += 1
-                    
+                print('Appended to csv sucessfully!')
         pageNum += 1
     except:
         RetryCounter = 1
