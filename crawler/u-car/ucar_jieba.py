43e0bb78dbd27f6fb293e0bbb02339130961bcc8
Eason Tse <eason.tse@eason-tse.me>
2015-12-22 15:35:44 +0800

    updated

--- crawler/u-car/ucar_jieba.py
+++ crawler/u-car/ucar_jieba.py
@@ -4,7 +4,7 @@
 # In[1]:
 
 import csv, psycopg2, jieba, operator
-jieba.load_userdict('autohome_dict.txt')
+jieba.load_userdict('ucar_dict.txt')
 import jieba.posseg as pseg
 
 jieba.enable_parallel()
@@ -18,11 +18,11 @@ def pseg_adj_n(row):
     dataset = [adj, n]
     return dataset
 
-autohome_csv_reader = csv.reader(open('autohome.csv'))
-autohome_new, nounlist = [], []
-with open('autohome_with_adj_n.csv', 'w') as csvfile:
+ucar_csv_reader = csv.reader(open('ucar.csv'))
+ucar_new, nounlist = [], []
+with open('ucar_with_adj_n.csv', 'w') as csvfile:
     textwriter = csv.writer(csvfile, delimiter=',')
-    for row in autohome_csv_reader:
+    for row in ucar_csv_reader:
         pseg_result = pseg_adj_n(row)
         
         textwriter.writerow([
@@ -31,8 +31,7 @@ with open('autohome_with_adj_n.csv', 'w') as csvfile:
             row[2],
             row[3],
             ' '.join(pseg_result[0]),
-            ' '.join(pseg_result[1]),
-            row[4]
+            ' '.join(pseg_result[1])
         ])
         nounlist.append(pseg_result[1])
 
@@ -44,7 +43,7 @@ for i in nounlist:
 word_count = {word:nounlist_unpack.count(word) for word in nounlist_unpack}
 word_count = sorted(word_count.items(),key=operator.itemgetter(1),reverse=True)
 
-with open('autohome_count.csv', 'w') as csvfile:
+with open('ucar_count.csv', 'w') as csvfile:
     textwriter = csv.writer(csvfile, delimiter=',')
     for word, count in word_count:
         textwriter.writerow([word, str(count)])
