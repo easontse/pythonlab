09d8b5f174c200329c7d8354efbea3cad629b8ad
Eason Tse <eason.tse@eason-tse.me>
2015-12-26 16:59:43 +0800

    autohome_jsonDump.py updated

--- data_analysis/autohome/autohome_jsonDump.py
+++ data_analysis/autohome/autohome_jsonDump.py
@@ -4,7 +4,7 @@ import gensim
 model = gensim.models.Word2Vec.load_word2vec_format('autohome_word_only.vector',binary=False)
 autohome_count = csv.reader(open('autohome_count.csv', 'r'))
 
-target_words = ['U6','车','纳智捷','优6','上市','感觉']
+target_words = ['U6','车','纳智捷','优6','价格','油耗']
 
 related_words = []
 for i in target_words:  
@@ -17,15 +17,31 @@ for i in related_words:
 
 count_dict = {row[0]:int(row[1]) for row in autohome_count if row[0] in related_words_unpack}
 
-a = []
+with open('autohome_with_adj_n.csv','r') as raw_data:
+    csv_reader = csv.reader(raw_data)
+    a = []
+    for row in csv_reader:
+        for word in related_words_unpack:
+            if word in row[5] and [word, row[2]] not in a:
+                a.append([word, row[2]])
+
+dict_list= {word:[i[1] for i in a if i[0] == word][:5] for word in related_words_unpack}
+
+dataset = []
 for i in target_words:
-    a.append(
+    dataset.append(
         {'name':i,
-         'children': [{'topic':x, 'rate':count_dict[x]} for x,_ in model.most_similar(i)[0:5]]
+         'children': [{'topic':x,
+                       'rate':count_dict[x], 
+                       'link':[
+                                {'title':y,'url':'https://www.google.ca/?gws_rd=ssl'} 
+                                for y in dict_list[x]
+                              ]
+                      } for x,_ in model.most_similar(i)[0:5]]
         }
     )
 
-print(json.dumps(a, indent=4, ensure_ascii=False))
+print(json.dumps(dataset, indent=4, ensure_ascii=False))
     
 with open('autohome.json','w') as outfile:
-    json.dumps(a, outfile, indent=4, ensure_ascii=False)
+    outfile.write(json.dumps(dataset, outfile, indent=4, ensure_ascii=False))
